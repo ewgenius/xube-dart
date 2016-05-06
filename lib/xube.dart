b1c8e25d4cd30264af2c2274a21cb9d146c08aab
ewgenius <ewgeniux@gmail.com>
2016-05-06 10:28:22 +0300

    lib

---
+++ lib/xube.dart
@@ -0,0 +1,5 @@
+library xube;
+
+class Game {
+  
+}
--- web/main.dart
+++ web/main.dart
@@ -1,8 +1,25 @@
 import 'dart:html';
 import 'dart:web_gl' as WebGL;
 import 'package:dartemis/dartemis.dart';
+//import 'package:xube_dart/xube.dart';
+//import 'package:dartemis_toolbox/system_transform.dart';
 
+class Transform extends Component {
+  num x, y, z;
+  Transform(this.x, this.y, this.z);
+}
 
+class TransformSystem extends EntityProcessingSystem {
+  TransformSystem(): super(Aspect.getAspectForAllOf([Transform]));
+
+  void initialize() {
+
+  }
+
+  void processEntity(Entity entity) {
+    print(entity);
+  }
+}
 
 void main() {
   CanvasElement canvas = querySelector('#canvas');
@@ -13,5 +30,11 @@ void main() {
   }
 
   World world = new World();
+  world.addSystem(new TransformSystem());
+
   Entity entity = world.createEntity();
+  entity.addComponent(new Transform(0, 0, 0));
+
+  world.initialize();
+  world.process();
 }
