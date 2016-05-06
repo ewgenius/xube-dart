f48aa6095abc02f3e26a167f4d57345002b4e685
ewgenius <ewgeniux@gmail.com>
2016-05-06 14:27:28 +0300

    base game class

--- lib/xube.dart
+++ lib/xube.dart
@@ -1,5 +1,54 @@
 library xube;
 
-class Game {
-  
+import 'dart:html';
+import 'dart:web_gl' as WebGL;
+import 'package:vector_math/vector_math.dart';
+import 'package:dartemis/dartemis.dart';
+
+class XubeGame {
+  bool _stop = false;
+  int _animFrameId;
+  double _elapsedTime;
+  World _world;
+  CanvasElement _canvas;
+  WebGL.RenderingContext _gl;
+
+  XubeGame(canvas) {
+    this._canvas = canvas;
+    this._gl = this._canvas.getContext("experimental-webgl");
+    if (canvas is! CanvasElement || _gl is! WebGL.RenderingContext) {
+      print("Failed to load canvas");
+    }
+
+    this._gl.clearColor(0, 0, 1, 1.0);
+    this._gl.enable(WebGL.RenderingContext.DEPTH_TEST);
+
+    this._world = new World();
+    this._world.initialize();
+  }
+
+  void run() {
+    print(this._gl);
+    this._renderFrame();
+  }
+
+  void _renderFrame() {
+    if (this._stop) {
+      window.cancelAnimationFrame(this._animFrameId);
+    } else {
+      this._animFrameId = window.requestAnimationFrame((double time) {
+        if (this._elapsedTime == null) {
+          this._elapsedTime = time;
+        }
+        this._render(time - this._elapsedTime);
+        this._elapsedTime = time;
+        this._renderFrame();
+      });
+    }
+  }
+
+  void _render([double time = .0]) {
+    this._gl.clear(WebGL.RenderingContext.COLOR_BUFFER_BIT |
+        WebGL.RenderingContext.DEPTH_BUFFER_BIT);
+  }
 }
--- web/main.dart
+++ web/main.dart
@@ -1,40 +1,8 @@
 import 'dart:html';
-import 'dart:web_gl' as WebGL;
-import 'package:dartemis/dartemis.dart';
-//import 'package:xube_dart/xube.dart';
-//import 'package:dartemis_toolbox/system_transform.dart';
-
-class Transform extends Component {
-  num x, y, z;
-  Transform(this.x, this.y, this.z);
-}
-
-class TransformSystem extends EntityProcessingSystem {
-  TransformSystem(): super(Aspect.getAspectForAllOf([Transform]));
-
-  void initialize() {
-
-  }
-
-  void processEntity(Entity entity) {
-    print(entity);
-  }
-}
+import 'package:xube_dart/xube.dart';
 
 void main() {
   CanvasElement canvas = querySelector('#canvas');
-  WebGL.RenderingContext gl = canvas.getContext("experimental-webgl");
-  if (canvas is! CanvasElement || gl is! WebGL.RenderingContext) {
-    print("Failed to load canvas");
-    return;
-  }
-
-  World world = new World();
-  world.addSystem(new TransformSystem());
-
-  Entity entity = world.createEntity();
-  entity.addComponent(new Transform(0, 0, 0));
-
-  world.initialize();
-  world.process();
+  XubeGame game = new XubeGame(canvas);
+  game.run();
 }
